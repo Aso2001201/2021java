f31042bc527931ba67527f92ec31bfb93292ea12
Aso2001201 <83050484+Aso2001201@users.noreply.github.com>
2021-07-15 10:57:20 +0900

    Add files via upload

---
+++ GameKadai.java
@@ -0,0 +1,10 @@
+public class GameKadai{
+	public static void main(String[] args){
+
+System.out.println("戦艦ゲーム");
+Scanner scanner = new Scanner(System.in);
+String here= scanner.next();
+    }
+}
+
+
