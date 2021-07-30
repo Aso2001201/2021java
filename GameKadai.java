8fcbebed70a86886a6edea089da29c49d31dbd21
Aso2001201 <83050484+Aso2001201@users.noreply.github.com>
2021-07-30 16:02:44 +0900

    Update GameKadai.java

--- GameKadai.java
+++ GameKadai.java
@@ -1,10 +1,27 @@
-public class GameKadai{
-	public static void main(String[] args){
+import java.util.Scanner;
 
-System.out.println("戦艦ゲーム");
-Scanner scanner = new Scanner(System.in);
-String here= scanner.next();
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Ship ship = new Ship();
+		
+        int s1 = 3;
+        int s2 = 3;
+        int s3 = 3;
+System.out.println(ss1);
+        int num = 0;
+        while(s1 != 0 || s2 != 0 || s3 != 0){
+            num++;
+            System.out.println("--------------[ターン"+num+"]--------------");
+
+            ship.dispb();
+
+            System.out.println("爆弾のx座標を入力してください(1-5)");
+            int x = sc.nextInt();
+            System.out.println("爆弾のy座標を入力してください(1-5)");
+            int y = sc.nextInt();
+
+            ship.dispa();
+        }
     }
 }
-
-
